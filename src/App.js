cc1e0ace502b87937eb7bcbd65ca3e2b45e8ced5
Serhat Zunluoğlu <97339604+serhatzunluoglu@users.noreply.github.com>
2023-03-11 16:09:59 +0300

    6.Hafta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { Table, Row,Col,Container} from "reactstrap";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      loading: true,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          users: response.data,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          loading: false,
+          error: error.message,
+        });
+      });
+  }
+
+  render() {
+    const { users, loading, error } = this.state;
+
+    // const users= this.state.users;
+    // const loading= this.state.loading;
+    // const error= this.state.error;
+
+    if (loading === true) {
+      return <p>Loading...</p>;
+    }
+
+    if (error === true) {
+      return <p>Error: {error}</p>;
+    }
+
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col>
+            <h1>Kullanıcı Listesi</h1>
+              <Table>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Username</th>
+                    <th>Name</th>
+                    <th>E-mail</th>
+                    <th>Telefon</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.map((user) => (
+                    <tr key={user.id}>
+                      <th>{user.id}</th>
+                      <td>{user.username}</td>
+                      <td>{user.name}</td>
+                      <td>{user.email}</td>
+                      <td>{user.phone}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
